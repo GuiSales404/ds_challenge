2c9ee27e5515e842cea56186615a4d990b2ca895
Guilherme Sales <61438749+GuiSales404@users.noreply.github.com>
2023-01-22 18:25:31 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,request, url_for, redirect, render_template, jsonify
+from pycaret.regression import *
+import pandas as pd
+import pickle
+import numpy as np
+
+# Initalise the Flask app
+app = Flask(__name__)
+
+# Loads pre-trained model
+model = load_model('models/Intelie_by_Viasat Challenge Model.pkl')
+
+cols = ['BDEP', 'TPO', 'HL', 'BHT', 'WOB']
+
+@app.route('/')
+def home():
+    return render_template("templates/home.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [x for x in request.form.values()]
+    final = np.array(int_features)
+    data_unseen = pd.DataFrame([final], columns = cols)
+    prediction = predict_model(model, data=data_unseen, round = 0)
+    prediction = int(prediction.Label[0])
+    if prediction == 0:
+        return render_template('home.html',pred='Expected situation will be that the slip is off')
+    if prediction == 1:
+        return render_template('home.html',pred='Expected situation will be that the slip is on')
+
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = predict_model(model, data=data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
